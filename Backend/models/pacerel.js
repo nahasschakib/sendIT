93e8d2d128d8647b57b71c3761edc433183ae2a0
sendIT <cnahass@gmail.com>
2024-07-16 17:53:09 +0100

    created autehtification

---
+++ Backend/models/pacerel.js
@@ -0,0 +1,19 @@
+const mongoose=require("mongoose");
+
+const parcelSchema=mongoose.Schema({
+    from:{type:String,require:true},
+    to:{type:String,require:true},
+    sendername:{type:String,require},
+    recipientname:{type:String,require:true},
+    senderemail:{type:String,require:true},
+    recipientemail:{type:String,require:true},
+    weight:{type:Number,require:true},
+    cost:{type:Number,require:true},
+    date:{type:String,require:true},
+    note:{type:String},
+    feedback:{type:String},
+    status:{type:Number,default:0},},{
+    timeStamp:true,
+})
+
+module.exports=mongoose.model("colis",parcelSchema)
